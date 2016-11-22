5b3cdada28f6c207300780bba0249850d38a41fe
lizhen <294474470@qq.com>
2016-11-22 16:47:24 +0800

    veracity test

--- bigchaindb/web/server.py
+++ bigchaindb/web/server.py
@@ -13,7 +13,7 @@ from bigchaindb import util
 from bigchaindb import Bigchain
 from bigchaindb.web.views.info import info_views
 from bigchaindb.web.views.transactions import transaction_views
-
+from bigchaindb.web.apiForVeracity.apiForVeracity import testVeracity_api
 from bigchaindb.monitor import Monitor
 
 
@@ -66,6 +66,7 @@ def create_app(settings):
 
     app.register_blueprint(info_views, url_prefix='/')
     app.register_blueprint(transaction_views, url_prefix='/api/v1')
+    app.register_blueprint(testVeracity_api, url_prefix='/testVeracity_api/v1')
     return app
 
 
---
+++ ul-tests/testVeracity/testVeracity01.py
@@ -0,0 +1,43 @@
+import requests
+import random
+import time
+import sys
+import multiprocessing as mp
+import os
+
+# post test
+
+
+processCount = sys.argv[1]
+
+url = 'http://127.0.0.1:9984/testVeracity_api/v1/write_tx/'
+headers = {'content-type': 'application/json'}
+
+def startRun():
+    postpid = os.getpid()
+    postcount = 0
+    while True:
+        random_transactions = random.Random().randint(500, 999)
+        sleep_random = random.Random().randint(1, 4)
+        print('will create %d transactions and then sleep %ds' % (random_transactions, sleep_random))
+        # starttimefor = datetime.datetime.now().microsecond
+        for i in range(random_transactions):
+            try:
+                postcount = postcount+1
+                requests.post(url, params={'postpid':postpid,'postcount':postcount}, headers=headers)
+                print("after"+postcount)
+            except:
+                print("%d has send %d posts" % (postpid,postcount))
+                # print("post except!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        # endtimefor = datetime.datetime.now().microsecond
+        # print('cost %d ms create %d transactions' % ((endtimefor-starttimefor)/1000,random_transactions))
+        time.sleep(sleep_random)
+
+
+if __name__ == "__main__":
+    pool = mp.Pool(processes=int(processCount))
+    for i in range(int(processCount)):
+        result = pool.apply_async(startRun)
+    pool.close()
+    pool.join()
+    print(result.successful())
