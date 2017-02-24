2bcd0a6327c8d9a9278482054067d1ea1ec15aec
lizhen <294474470@qq.com>
2017-02-24 15:29:55 +0800

    test files

--- ul_tests/apiForDemoTest/api_createElectricTrans.py
+++ ul_tests/apiForDemoTest/api_createElectricTrans.py
@@ -1,19 +1,115 @@
 import json
 import requests
 
-url='http://127.0.0.1:9984/uniledger/v1/block/queryByID/'
-url='http://127.0.0.1:9984/uniledger/v1/timestat/txCreateAvgTime'
-values = """{
-    "block_id":"0163c975ef21cfdcd282f3dd9021800e7d2f17befc5f4a20aed91b22a04cb4e0",
-}"""
+# # url='http://192.168.159.100:9984/uniledger/v1/block/queryByID/'
+# values = {
+#                 "block_id": "f41591dbc01c5a7f016dc43c0044ab6f9e93e21e378ed147e91f66ae75e2b638"
+#             }
+
+
+# url='http://192.168.159.100:9984/uniledger/v1/block/queryTxsByID/'
+#
+# values = {
+#                 "block_id": "f41591dbc01c5a7f016dc43c0044ab6f9e93e21e378ed147e91f66ae75e2b638"
+#             }
+#
+# url='http://192.168.159.100:9984/uniledger/v1/block/queryTxsCountByID/'
+#
+# values = {
+#                 "block_id": "f41591dbc01c5a7f016dc43c0044ab6f9e93e21e378ed147e91f66ae75e2b638"
+#             }
+
+# url='http://192.168.159.100:9984/uniledger/v1/block/queryBlockCount/'
+
+# url='http://192.168.159.100:9984/uniledger/v1/block/queryBlocksByRange/'
+#
+# values = {
+#                 "startTime":"1483689947",
+#                 "endTime": "1487230112918"
+#             }
+
+# url='http://127.0.0.1:9984/uniledger/v1/block/queryInvalidBlockTotal/'
+
+
+# url='http://127.0.0.1:9984/uniledger/v1/block/queryInvalidBlockByRange/'
+#
+# values = {
+#                 "beginTime":"1487230113051",
+#                 "endTime": "1487230113055"
+#             }
+
+
+#vote=========================
+# url='http://192.168.159.100:9984/uniledger/v1/vote/publickeySet/'
+
+
+#timestat=========================
+# url='http://192.168.159.100:9984/uniledger/v1/timestat/txCreateAvgTime'
+# values = {
+#                 "begintime":"1487230111131",
+#                 "endtime": "1487230112918"
+#             }
+
+
+# url='http://192.168.159.100:9984/uniledger/v1/timestat/blockCreateAvgTime'
+# values = {
+#                 "begintime":"1487230111131",
+#                 "endtime": "1487230112918"
+#             }
+#
+# url='http://192.168.159.100:9984/uniledger/v1/timestat/voteTimeByBlockID'
+# values = {
+#                 "id": "93a2a544eb0971c8b0a910afa22b638437fc85fe10cdd2537a3872c03cce7be7"
+#             }
+
+# url='http://36.110.115.195:38/uniledger/v1/timestat/voteTimeAvgTime'
+# values = {
+#                 "beginTime":"0",
+#                 "endTime": "1487396838848"
+#             }
+
+# url='http://36.110.115.195:38/uniledger/v1/timestat/voteTimeAvgTime'
+# values = {
+#                 "beginTime":"0",
+#                 "endTime": "1487396838848"
+#             }
+
+# url='http://127.0.0.1:9984/uniledger/v1/transaction/queryByID/'
+# values = {
+#                 "tx_id": "39d8d3d29554d209a1283b20a1e3e198bdd27c099b8df9042195d7bb0728219f",
+#                 "type":"1"
+#             }
+
+# url='http://127.0.0.1:9984/uniledger/v1/condition/getAsset?unspent=true&public_key={}'.format('6GSFmL4kcK6YJVFC2xq1KegmezhMRNhXLGmRAkLEt9Zq')
+# values = {
+#                 "public_key": "",
+#                 "unspent":True
+#             }
+
+
+#alice create tx
+# url='http://127.0.0.1:9984/uniledger/v1/transaction/createByUser/'
+# values = {
+#                 "payload":"test"
+#             }
+
+url='http://127.0.0.1:9984/uniledger/v1/transaction/getTxRecord?public_key={}'.format('Gvexu49oskc6ptYwzqP9q8sL9jLxjNZNMBWgVVhUtPmD')
+
+
+
+r=requests.get(url)
+
+print(r.text)
+
+
 
 
 headers = {
   'Content-Type': 'application/json'
 }
-value=json.dumps(values)
-r=requests.post(url,data=value,headers=headers)
-print(r.text)
+# value=json.dumps(values)
+# r=requests.post(url,data=value,headers=headers)
+# print(r.text)
 
 
-# curl -i -X POST -H 'Content-Type:application/json' -d '{}' http://127.0.0.1:9984/uniledger/v1/block/queryByID//
+
--- ul_tests/apiForDemoTest/test.py
+++ ul_tests/apiForDemoTest/test.py
@@ -56,17 +56,51 @@ def get_txNumberById(block_id):
     # s = r.table("bigchain").concat_map(lambda block: block['block']['transactions']).order_by(r.asc(r.row['block']['transactions']['transaction']['timestamp'])).filter((r.row["transaction"]['timestamp'] < '1487076389680') & (r.row["transaction"]['timestamp'] > '1487076389678')).run(conn)
     # r.table('users').get(10001).pluck({'contact': {'phone': 'work'}}
     # s = r.table("bigchain").with_fields('id':r.row['block']['transactions']).run(conn)
-    s = r.table("bigchain").order_by(index=r.desc('block_timestamp')).map({'id':r.row['id'],'count':r.row['block']['transactions'].count()}).limit(10).run(conn)
+    # s = r.table("bigchain").order_by(index=r.desc('block_timestamp')).map({'id':r.row['id'],'count':r.row['block']['transactions'].count()}).limit(10).run(conn)
     # r.db('bigchain').table('bigchain').limit(20).map(id: row('id'), signature: row('signature'), len: row('block')('transactions').count()};
     # for i in s:
     #     id = i['id']
     #     l = len(i['transactions'])
     #     print(l)
+
+    # s = r.table('bigchain').between('1487230111131', '1487230111830', index='tx_timestamp').run(conn)  # tx time
+    # s = r.table('bigchain').get_field('block').get_field('transactions').run(conn)
+    #s = r.table("bigchain").concat_map(lambda block: block['block']['transactions']).filter((r.row["transaction"]['timestamp'] > '1487230111131') & (r.row["transaction"]['timestamp'] < '1487230111831')).count().run(conn)
+    #s = r.table('bigchain').between('1487230111131', '1487230112918', index='block_timestamp').count().run(conn)
+    # s = r.table('bigchain').get_all(block_id, index='id').run(conn)
+    # s = r.table('bigchain').get_all(block_id, index='id').get_field('block').get_field('timestamp').run(conn)
+    # s = r.table('votes').filter(r.row['vote']['voting_for_block'] == block_id).max(r.row['vote']['timestamp']).get_field('vote').get_field('timestamp').run(conn)
+    # s = r.table('votes').between('0', '1', index='block_and_voter').get_field('vote').get_field('voting_for_block').distinct().count().run(conn)
+    # s = r.table('bigchain').get_all(block_id, index='id').get_field('block').get_field('transactions').run(conn)
+    # s = r.table("bigchain").concat_map(lambda block: block['block']['transactions']).order_by(r   .desc(r.row['block']['transactions']['transaction']['timestamp'])).filter((r.row["transaction"]['timestamp'] >= '1487313303289') and (r.row["transaction"]['timestamp'] <= '1487313304289')).run(conn)
+    # s = r.table("bigchain").between('1487313303288', '1487313304299', index='tx_timestamp').get_field('block').get_field('transactions').run(conn)
+    # s = r.table('bigchain').get_all('39d8d3d29554d209a1283b20a1e3e198bdd27c099b8df9042195d7bb0728219f', index='transaction_id').run(conn)
+
+    pubkey = 'Gvexu49oskc6ptYwzqP9q8sL9jLxjNZNMBWgVVhUtPmD'
+    s = r.table('bigchain').concat_map(lambda doc: doc['block']['transactions'])\
+        .filter(lambda tx: (tx['transaction']['conditions'].contains(lambda c: c['owners_after'].contains(pubkey)))
+                        or (tx['transaction']['fulfillments'].contains(lambda f: f['owners_before'].contains(pubkey))))\
+        .order_by(r.row['timestamp']).map({'id':r.row['id'],'owner_before':r.row['transaction']['fulfillments']['owners_before'],'operation':r.row['transaction']['operation'],'conditions':r.row['transaction']['conditions'],'timestamp':r.row['transaction']['timestamp']}).run(conn)
+
+
+    # s = r.table('rewrite').between('', '', index='block_timestamp').get_field('id').run(conn)
     print(s)
 
-get_txNumberById("e5ff34ef72fd80e839512652c4a9c55764e648590a05c5feb88610a6a5e0e491")
+get_txNumberById("39d8d3d29554d209a1283b20a1e3e198bdd27c099b8df9042195d7bb0728219f")
 
 # -----------------------------------
 # r.table('rewrite').index_create('block_timestamp',r.row['timestamp']).run(conn)
 # -----------------------------------
+#
 # r.table('bigchain').index_create('tx_timestamp', r.row['block']['transactions']['transaction']['timestamp']).run(conn)
+
+
+# from bigchaindb.common.util import gen_timestamp
+# print(isinstance(gen_timestamp(),str))
+# from flask import jsonify
+# avgtime_d= {'avgtime':'q'}
+# s = jsonify({
+#         'status':'1',
+#         'data':avgtime_d
+#     })
+# print(s)
