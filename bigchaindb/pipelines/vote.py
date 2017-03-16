c2c44799759d2fecd123cb8c66241400381265ee
KURO1 <412681778@qq.com>
2017-03-16 12:47:44 +0800

    configure: vote argument

--- bigchaindb/pipelines/vote.py
+++ bigchaindb/pipelines/vote.py
@@ -173,7 +173,7 @@ def create_pipeline():
     vote_pipeline = Pipeline([
         Node(voter.validate_block),
         Node(voter.ungroup),
-        Node(voter.validate_tx, fraction_of_cores=config['argument_config']['fraction_of_cores']),
+        Node(voter.validate_tx, fraction_of_cores=config['argument_config']['vote_pipeline.fraction_of_cores']),
         Node(voter.vote),
         Node(voter.write_vote)
     ])
