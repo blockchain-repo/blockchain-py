5058231c654dbd49cde9ad7c8186bd533da1562c
kuro1 <412681778@qq.com>
2017-10-26 12:14:14 +0800

    handle Block.from_dict() raise in election

--- bigchaindb/pipelines/election.py
+++ bigchaindb/pipelines/election.py
@@ -7,15 +7,17 @@ is specified in ``create_pipeline``.
 import logging
 
 import rethinkdb as r
+from bigchaindb.common.exceptions import InvalidHash
 from multipipes import Pipeline, Node
 from bigchaindb.pipelines.utils import ChangeFeed
-from bigchaindb.models import Block
-from bigchaindb import Bigchain,config
-from time import sleep,time
+from bigchaindb.models import Block, Transaction
+from bigchaindb import Bigchain, config
+from time import sleep, time
 from bigchaindb.monitor import Monitor
 
 logger = logging.getLogger(__name__)
 monitor = Monitor()
+
 
 class Election:
     """Election class."""
@@ -32,37 +34,49 @@ class Election:
 
         """
         next_block = self.bigchain.connection.run(
-                r.table('bigchain')
+            r.table('bigchain')
                 .get(next_vote['vote']['voting_for_block']))
 
         if monitor is not None:
             with monitor.timer('validate_vote'):
                 block_status = self.bigchain.block_election_status(next_block['id'],
-                                                           next_block['block']['voters'])
+                                                                   next_block['block']['voters'])
         else:
             block_status = self.bigchain.block_election_status(next_block['id'],
-                                                           next_block['block']['voters'])
+                                                               next_block['block']['voters'])
 
-        #block_status = self.bigchain.block_election_status(next_block['id'],
+        # block_status = self.bigchain.block_election_status(next_block['id'],
         #                                                   next_block['block']['voters'])
-        logger.debug('Blocke %s election status %s',next_block['id'] ,block_status)
+        logger.debug('Blocke %s election status %s', next_block['id'], block_status)
         if block_status == self.bigchain.BLOCK_UNDECIDED:
             return
         # write to decide_block  next_block['id'] ,block_status , txs_count
-        self.bigchain.save_block_status(next_block['block']['node_pubkey'],next_block['id'],block_status,len(next_block['block']['transactions']))
+        self.bigchain.save_block_status(next_block['block']['node_pubkey'], next_block['id'], block_status,
+                                        len(next_block['block']['transactions']))
         if block_status == self.bigchain.BLOCK_INVALID:
-            return Block.from_dict(next_block)
+            try:
+                block = Block.from_dict(next_block)
+                return block
+            except InvalidHash:
+                logger.debug("InvalidHash %s", next_block['id'])
+                transactions = [Transaction.from_dict(tx) for tx
+                                in next_block['block']['transactions']]
+                signature = next_block['signature']
+                return Block(transactions, next_block['block']['node_pubkey'],
+                             next_block['block']['timestamp'], next_block['block']['voters'], signature)
 
     def requeue_transactions(self, invalid_block):
         """
         Liquidates transactions from invalid blocks so they can be processed again
         """
         isHandled = self.bigchain.selectFromWrite(invalid_block.id)
-        if self.bigchain.me == invalid_block.node_pubkey and isHandled ==False:
-            logger.info('Rewriting %s transactions from invalid block %s', len(invalid_block.transactions), invalid_block.id)
+        if self.bigchain.me == invalid_block.node_pubkey and isHandled == False:
+            logger.info('Rewriting %s transactions from invalid block %s', len(invalid_block.transactions),
+                        invalid_block.id)
             if monitor is not None:
                 monitor.incr('rewrite_block', 1)
-            data = {'id':invalid_block.id,'node_publickey': invalid_block.node_pubkey,'timestamp':invalid_block.timestamp}
+            data = {'id': invalid_block.id, 'node_publickey': invalid_block.node_pubkey,
+                    'timestamp': invalid_block.timestamp}
             self.bigchain.insertRewrite(data)
             for tx in invalid_block.transactions:
                 self.bigchain.write_transaction(tx)
@@ -70,19 +84,21 @@ class Election:
         # 不是当前节点建的块才返回，进入下一个node
         return invalid_block
 
-    def check_local_mem(self,invalid_block):
+    def check_local_mem(self, invalid_block):
         isHandled = self.bigchain.selectFromWrite(invalid_block.id)
         if not isHandled:
             nodeIndex = self.bigchain.nodelist.index(invalid_block.node_pubkey)
             myIndex = self.bigchain.nodelist.index(self.bigchain.me)
             # 计算到什么时间才需要当前node处理
             if nodeIndex < myIndex:
-                endtime = time() + (myIndex - nodeIndex ) * config['argument_config']['election_pipeline.wait_time'] # 每个节点需要n秒钟处理时间，待定为10s
+                endtime = time() + (myIndex - nodeIndex) * config['argument_config'][
+                    'election_pipeline.wait_time']  # 每个节点需要n秒钟处理时间，待定为10s
             else:
-                endtime = time() + (len(self.bigchain.nodelist) - nodeIndex + myIndex) * config['argument_config']['election_pipeline.wait_time']
+                endtime = time() + (len(self.bigchain.nodelist) - nodeIndex + myIndex) * config['argument_config'][
+                    'election_pipeline.wait_time']
 
             # 在到截止时间的过程中，不断的去查询这个block是否已经处理了。
-            while(endtime > time()):
+            while (endtime > time()):
                 sleep(3)
                 isHandled = self.bigchain.selectFromWrite(invalid_block.id)
                 if isHandled:
@@ -91,12 +107,14 @@ class Election:
             logger.info(' Rewriting %s transactions from invalid block %s --check_local_mem',
                         len(invalid_block.transactions),
                         invalid_block.id)
-            data = {'id': invalid_block.id, 'node_publickey': invalid_block.node_pubkey,'timestamp':invalid_block.timestamp}
+            data = {'id': invalid_block.id, 'node_publickey': invalid_block.node_pubkey,
+                    'timestamp': invalid_block.timestamp}
             self.bigchain.insertRewrite(data)
             for tx in invalid_block.transactions:
                 self.bigchain.write_transaction(tx)
             return invalid_block
         return
+
 
 def get_changefeed():
     return ChangeFeed(table='votes', operation=ChangeFeed.INSERT)
